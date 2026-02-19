9dc3124dc5afe355533ba3f885995d4294670507
garvanant17 <150727837+garvanant17@users.noreply.github.com>
2026-02-19 16:09:36 +0530

    Update rectanglecalculator.cpp

--- rectanglecalculator.cpp
+++ rectanglecalculator.cpp
@@ -1,55 +1,33 @@
 #include <iostream>
-#include <iomanip>
 using namespace std;
-class BankAccount{
-string accountHolderName;
-int AccountNumber;
-double balance;
+class Rectangle{
+float length;
+float width;
+//float area;
+//float perimeter;
 public:
-void inputDetails(){
-
-    cout<<"enter holder name: ";
-    getline(cin, accountHolderName);
-    cout<<"enter account number: ";
-    cin>>AccountNumber;
-    cout<<"enter balance: ";
-    cin>>balance;
+void inputDimension(){
+    cout<<"enter length and width: ";
+    cin>>length>>width;
+    //calculateArea();
+    //calculatePerimeter();
 }
-void displaydetails(){
-
-    cout<<endl<<"name: "<<accountHolderName<<endl<<"account number: "<<AccountNumber<<endl;
-    cout<<fixed<<setprecision(2);
-    cout<<"balance: "<<balance;
+void displayResult(){
+    cout<<"area: "<<calculateArea()<<endl<< "perimeter: "<<calculatePerimeter();
 }
-
-void deposit(double amount){
-    cout<<fixed<<setprecision(2);
-    balance = balance + amount;
-    cout<<"balance is: "<<balance;
+private:
+float calculateArea(){
+    return length*width;
+    //return area;
 }
-void withdraw(double amount){
-    if(amount>balance)
-    cout<<"insufficient balance";
-    else if(amount <= balance ){
-        cout<<fixed<<setprecision(2);
-        balance = balance - amount;
-        cout<<"balance: "<<balance;
-    }
-
+float calculatePerimeter(){
+     return 2*(length+width);
+    //return perimeter;
 }
-
 };
 
 int main(){
-    BankAccount b1;
-    double amount;
-    b1.inputDetails();
-    cout<<"enter amount you want to deposit: ";
-    cin>>amount;
-    b1.deposit(amount);
-    cout<<endl<<"enter amount you want to withdraw: ";
-    cin>>amount;
-    b1.withdraw(amount);
-    cout<<endl;
-    b1.displaydetails();
+    Rectangle r1;
+    r1.inputDimension();
+    r1.displayResult();
 }
